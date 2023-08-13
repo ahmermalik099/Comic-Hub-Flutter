da9e6f72cd3e9c7abbca815512b8d7b256633c4c
ahmermalik099 <ahmermalik099@gmail.com>
2023-08-14 01:37:02 +0500

    from mr

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: '/',
       getPages: [
         GetPage(
@@ -30,6 +31,7 @@ class MyApp extends StatelessWidget {
           page: () => DetailsScreen(),
           transition: Transition.fadeIn,
         ),
+        //come
       ],
     );
   }
